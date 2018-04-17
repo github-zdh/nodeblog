61f2f6a137f0d2506442a68ab5959bb5d549fbfd
nodejs <2056401340@qq.com>
2018-04-17 11:35:43 +0800

    .

--- web/controller/index.js
+++ web/controller/index.js
@@ -103,7 +103,11 @@ obj.getSearchList=function (req, res, next) {
 
 // 获取所有的文章/帖子列表
 obj.getArtList=function (req, res, next) {
-
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+      res.header("X-Powered-By",' 3.2.1')
+      res.header("Content-Type", "application/json;charset=utf-8");
       var query = req.query;
       var limitCount=query.limit||30;
       var page=(query.curr-1)*limitCount;
