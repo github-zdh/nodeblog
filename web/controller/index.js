49c1758beea66e37f9815a01c6211879a0f6f436
Mini Program <35329223+github-zdh@users.noreply.github.com>
2018-05-07 18:17:21 +0800

    Update index.js

--- web/controller/index.js
+++ web/controller/index.js
@@ -311,10 +311,9 @@ obj.addArtContent=function (req, res, next) {
 		                     " update_time = " +  com.timestamp()+","+
 		                     " content = " +  "'"+query.content+"',"+
 		                     " img = " + "'"+ query.img+"',"+
-		                     " description = " + ("`" + query.description +"`").toString() + ","+
+		                     " description = " + """" + query.description +"""" + ","+
 				     " where id = "+ query.artListId ;
-                                  console.log(("`" + query.description +"`").toString());
-			  console.log('------------------------')
+			  
 				  sql.runSql(getArtUserId,function(err,data){
 					  	  // console.log(data);
 					  	  if(err){
