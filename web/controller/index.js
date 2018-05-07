8676d7f9966d3ddc9f533d15dad5e0050d411a8a
Mini Program <35329223+github-zdh@users.noreply.github.com>
2018-05-07 18:06:05 +0800

    Update index.js

--- web/controller/index.js
+++ web/controller/index.js
@@ -311,14 +311,9 @@ obj.addArtContent=function (req, res, next) {
 		                     " update_time = " +  com.timestamp()+","+
 		                     " content = " +  "'"+query.content+"',"+
 		                     " img = " + "'"+ query.img+"',"+
-		                     " description = " +  "`"+query.description.indexOf("'")>-1?query.description.replace(/\'/g,"\'"):query.description+"`".replace(/`/g,"'")+
+		                     " description = " + (("`"+(query.description.indexOf("'")>-1?query.description.replace(/\'/g,"\'"):query.description)+"`").replace(/`/g,"'")) +
 				             " where id = "+ query.artListId ;
-console.log('---------------------------------------');
-console.log("`"+123+"`");
-console.log("`"+(query.description.indexOf("'")>-1?query.description.replace(/\'/g,"\'"):query.description)+"`");
-console.log('---------------------------------------');
-console.log("'`"+query.description.indexOf("'")>-1?query.description.replace(/\'/g,"\'"):query.description+"'`".replace(/\'`/g,"'"));
-console.log('---------------------------------------');
+
 				  sql.runSql(getArtUserId,function(err,data){
 					  	  // console.log(data);
 					  	  if(err){
