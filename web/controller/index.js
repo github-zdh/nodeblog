c7bfd57be9e5fc4ff8240ba3da77d80080eca759
Mini Program <35329223+github-zdh@users.noreply.github.com>
2018-05-07 18:04:28 +0800

    Update index.js

--- web/controller/index.js
+++ web/controller/index.js
@@ -315,7 +315,7 @@ obj.addArtContent=function (req, res, next) {
 				             " where id = "+ query.artListId ;
 console.log('---------------------------------------');
 console.log("`"+123+"`");
-console.log("'`"+query.description.indexOf("'")>-1?query.description.replace(/\'/g,"\'"):query.description+"'`");
+console.log("`"+(query.description.indexOf("'")>-1?query.description.replace(/\'/g,"\'"):query.description)+"`");
 console.log('---------------------------------------');
 console.log("'`"+query.description.indexOf("'")>-1?query.description.replace(/\'/g,"\'"):query.description+"'`".replace(/\'`/g,"'"));
 console.log('---------------------------------------');
