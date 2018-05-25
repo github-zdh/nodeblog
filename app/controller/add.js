d96a0fafa3f641ccd3445e9b49ff526c436235cb
nodejs <2056401340@qq.com>
2018-05-25 18:15:17 +0800

    .

--- app/controller/add.js
+++ app/controller/add.js
@@ -9,8 +9,10 @@ var base = require(__ROOTDIR__+'/config/base');
 
 // 查询用户
 exports.friend=function (req, res, next) {
+       var uid = req.body.uid;
+       var fid = req.body.fid;
        function addFriend(uid,fid){
-		          var email = 'insert into z_friends (uid,uids) values ('+uid+','+fid+')';
+		          var email = 'insert into z_friends (uid,fid) values ('+uid+','+fid+')';
 		          sql.runSql(email,function(err,data){
 				     	    if(err){
 				     	    	    return base.returnjson(res,100,"查询失败");
@@ -19,37 +21,18 @@ exports.friend=function (req, res, next) {
 		          })
        }
        new sql.runMysql(()=>{
-       	    return base.returnjson(res,100,"查询失败");
+       	    return base.returnjson(res,1002,"查询失败");
         } )
        .then(()=>{
-       	    return 'select * from z_friends where uid ="'+req.body.uid+'"';
+       	    return 'select * from z_friends where uid ='+ uid +' and fid = '+ fid;
         },
        	(data,_this)=>{
      	    if(data.length==0){
-                   addFriend(req.body.uid,req.body.fid);
+                   addFriend(uid,fid);
 		          _this.endHandle = true;
-     	    }
-       	})
-       .then('select uids from z_friends where uid ='+ req.body.uid,
-       	(data,_this)=>{
-     	    if(data[0].uids == ''){
-		           addFriend(req.body.uid,req.body.fid);
      	    }else{
-	     	     var uids = String(data[0].uids).split(',');
-	     	     var strFid = String(req.body.fid);
-	     	     if(uids.indexOf(strFid)>-1){
-	     	     	  return base.returnjson(res,200,'你已经添加该用户了');
-	     	     }else{
-                      uids.push(strFid);
-			          var email = 'UPDATE z_friends set uids = "'+uids.join(',')+'" where uid = '+req.body.uid;
-			          sql.runSql(email,function(err,data){
-					     	    if(err){
-					     	    	    return base.returnjson(res,100,"查询失败");
-					     	    }
-					            return base.returnjson(res,200,'添加成功');
-			          })
-	     	     }
-     	    }            
+     	    	 return base.returnjson(res,101,'你已经添加该用户了');
+     	    }
        	})
        .end();
 };
