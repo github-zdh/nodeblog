ee30d5bb67e5f18cbe71d9ecbfb8d97c5419893b
nodejs <2056401340@qq.com>
2018-05-25 15:04:27 +0800

    .

--- app/controller/add.js
+++ app/controller/add.js
@@ -9,7 +9,16 @@ var base = require(__ROOTDIR__+'/config/base');
 
 // 查询用户
 exports.friend=function (req, res, next) {
-
+       function addFriend(uid,fid){
+		          var email = 'insert into z_friends (uid,uids) values ('+uid+','+fid+')';
+		          console.log(email);
+		          sql.runSql(email,function(err,data){
+				     	    if(err){
+				     	    	    return base.returnjson(res,100,"查询失败");
+				     	    }
+				            return base.returnjson(res,200,'添加成功');
+		          })
+       }
        new sql.runMysql(()=>{
        	    return base.returnjson(res,100,"查询失败");
         } )
@@ -18,26 +27,25 @@ exports.friend=function (req, res, next) {
         },
        	(data,_this)=>{
      	    if(data.length==0){
-		          var email = 'insert into z_friends (uid,uids) values ('+req.body.uid+','+req.body.fid+')';
-		          console.log(email);
-		          sql.runSql(email,function(err,data){
-				     	    if(err){
-				     	    	    return base.returnjson(res,100,"查询失败");
-				     	    }
-				            return base.returnjson(res,200,'添加成功');
-		          })
+                   addFriend(req.body.uid,req.body.fid);
 		          _this.endHandle = true;
      	    }
      	    console.log(2);
        	})
        .then('select uids from z_friends where uid ='+ req.body.uid,
        	(data,_this)=>{
-     	    console.log(3);
-     	    console.log('select uids from z_friends where uid ="'+req.body.uid+'"');
-     	    if(data.length==0){
-     	    	    return base.returnjson(res,100,"用户不存在");
-     	    }
-            return base.returnjson(res,200,'查询成功',data[0]);
+     	    if(data[0].uids == ''){
+		           addFriend(req.body.uid,req.body.fid);
+     	    }else{
+	     	     var uids = String(data[0].uids).split(',');
+	     	     var strFid = String(req.body.fid);
+	     	     if(uids.indexOf(strFid)>-1){
+	     	     	  return base.returnjson(res,200,'你已经添加该用户了');
+	     	     }else{
+                      uids.push(strFid);
+			          addFriend(req.body.uid,uids.join(','));
+	     	     }
+     	    }            
        	})
        .end();
 };
