10afc2349f9d423191a92ccd47cfbcc769389638
nodejs <2056401340@qq.com>
2018-05-29 11:11:06 +0800

    .

--- io/room.js
+++ io/room.js
@@ -84,10 +84,22 @@ const room = function(){
                           resolve({code:200,msg:"查询成功",result:data[0]});
                       })
               }
+              // 读取房间信息
+              var getRoomInfo = function(resolve,reject,rid){
+                      var email = 'select r.*,u.uid,u.description as udescriptio from z_room r left join z_user_room u on u.rid=r.id where r.id="'+rid+'"';
+                      sql.runSql(email,function(err,data){
+                          if(err){
+                                var info = global.ioRooms[rid] ? global.ioRooms[rid].info:{};
+                                resolve({code:100,msg:"查询失败",result:info});
+                                return false;
+                          }
+                          resolve({code:200,msg:"查询成功",result:data});
+                      })
+              }
               // 加入房间
               socket.on('room join',function(obj){roomJoin(obj)});
               var roomJoin = async (obj) => {
-                    console.log(obj);
+                    // console.log(obj);
                     if(obj&&obj.rid){
                         rid = obj.rid;
                     }
@@ -99,43 +111,144 @@ const room = function(){
                     userInfo = await asyncAwait(function(resolve,reject){
                          getUserInfo(resolve,reject,uid)
                     });
-                    console.log(userInfo);
-                    console.log('============')
+                    userInfo = userInfo['result'];
+                    // console.log(userInfo);
+                    // console.log('============')
+                    // 添加房间
                     if(!global.ioRooms[rid]){
-                         global.ioRooms[rid] = [];
+                           global.ioRooms[rid] = {
+                                 info:{},
+                                 room:[]
+                           };
                     }
-                    if(ioRooms[rid].indexOf(userInfo)==-1){
-                        ioRooms[rid].push(userInfo);
+                     global.ioRooms[rid].info =  await asyncAwait(function(resolve,reject){
+                           getRoomInfo(resolve,reject,rid)
+                     });
+
+                    if(ioRooms[rid].room.indexOf(userInfo)==-1){
+                        ioRooms[rid].room.push(userInfo);
                         socket.join(rid);
                     }
                     // console.log('----------------ioRooms---------------------');
                     // console.log(ioRooms);
-                    room.to(rid).emit('room sys', userInfo['result']['username'] + '加入', ioRooms , ioRooms[rid]);
+                    room.to(rid).emit('room sys', userInfo['username'] + '加入', ioRooms , ioRooms[rid]);
+
+                    // 发送未读消息
+                    // console.log(unread());
+                    sendRoomUnread();
+                    
               }
               
+              // 读取该用户 存储未读消息
+              var getStoreUnread = function(resolve,reject){
+                      var email = 'SELECT msg  from z_room_unread where status = 0 and uid='+uid+' and rid='+rid+' order by addtime ASC';
+                      // console.log(email);
+                      sql.runSql(email,function(err,data){
+                            if(err){
+                                  resolve([]);
+                                  return false;
+                            }
+                            resolve(data);
+                      })
+              }
+              // 修改该用户 存储未读消息 状态
+              var reStoreUnread = function(){
+                      var email = 'update z_room_unread set status = 1 where uid='+uid+' and rid='+rid;
+                      // console.log(email);
+                      sql.runSql(email,function(err,data){
+                            if(err){
+                                  return false;
+                            }
+                            return false;
+                      })
+              }
               //roomJoin();   
               // 发送未读消息
-              const unread = () => {
-                   return chatRecord[rid];
+              const sendRoomUnread = async () => {
+                    var getUnread = await asyncAwait(function(resolve,reject){
+                         getStoreUnread(resolve,reject);
+                    })
+                    reStoreUnread();
+                    var newUnreadMsg = [];
+                    for(var i=0;i<getUnread.length;i++){
+                        newUnreadMsg.push(getUnread[i].msg);
+                    }
+
+                    socket.emit('room unread',newUnreadMsg);
               }
-              socket.emit('room unread',unread());
+
+              
+
+              // 检查不在线人并且把信息存到数据库  存储未读消息
+              var storeUnread = function(resolve,reject,rid,uid,msg,addtime){
+                      var roomArr = [];
+                      for(var i=0;i<ioRooms[rid]['info']['result'].length;i++){
+                           roomArr.push(ioRooms[rid]['info']['result'][i].uid)
+                      }
+                      var uArr = [];
+                      for(var i=0;i<ioRooms[rid].room.length;i++){
+                           uArr.push(ioRooms[rid].room[i].id)
+                      }
+                      Array.prototype.diff = function(a){
+                           return this.filter(function(i){return a.indexOf(i) < 0 ; })
+                      }
+                      var uroom = roomArr.diff(uArr);
+                      var insertValues = ''
+                      for(var i=0;i<uroom.length;i++){
+                           if(i==0){
+                                 insertValues = '('+rid+','+uroom[i]+',\''+msg+'\','+addtime+')';
+                           }else{
+                                 insertValues = insertValues + ','+ '('+rid+','+uroom[i]+',\''+msg+'\','+addtime+')';
+                           }
+                      }
+                      var email = 'insert into z_room_unread (rid,uid,msg,addtime) VALUES '+insertValues;
+                      // console.log(email);
+                      sql.runSql(email,function(err,data){
+                            if(err){
+                                  resolve({code:100,msg:"查询失败",result:[]});
+                                  return false;
+                            }
+                            resolve({code:200,msg:"查询成功",result:data});
+                      })
+              }
+              // 存储所有的聊天记录
+              var storeAllChatMsg = function(addtime,msg){
+                      var email = 'insert into z_room_msg (rid,msg,addtime) VALUES ('+rid+',\''+msg+'\','+addtime+')';
+                      // console.log(email);
+                      sql.runSql(email,function(err,data){
+                            if(err){
+                                  return false;
+                            }
+                            return false;
+                      })
+              }
 
               // 接收发的信息
-              socket.on('room sendMsg',function(data,callback){
+              socket.on('room sendMsg', async function(data,callback){
                     // 给某个房间触发信息
                     var smsg = {
                             msg:data.sendMsg,//信息内容
-                            from:userInfo['result'],// 谁发送
+                            from:userInfo,// 谁发送
                             status:false,//false=>未读 true =>已读
                             type:'text',//信息类型
-                            times:new Date().getTime() //发送时间
+                            times:parseInt(new Date().getTime()/1000) //发送时间
                     }
-                    console.log(chatRecord);
-                    if(!chatRecord[rid]){
-                         chatRecord[rid] = [];
-                    }
-                    chatRecord[rid].push(smsg)
+                     
+                    // console.log(JSON.stringify(smsg));
+
                     room.to(rid).emit('room msg', smsg );
+
+                    storeAllChatMsg(smsg.times,JSON.stringify(smsg));
+
+                    // 存储未读消息
+                     await asyncAwait(function(resolve,reject){
+                           storeUnread(resolve,reject,rid,uid,JSON.stringify(smsg),smsg.times)
+                     });
+                     
+                    // var getStoreUnread = await asyncAwait(function(resolve,reject){
+                    //        storeUnread(resolve,reject,rid,uid,msg)
+                    //  });
+
                     if(callback){callback(data)};
               })
               //断网或者离开当前聊天页面
@@ -146,8 +259,10 @@ const room = function(){
                     // console.log(ioRooms[rid]);
                     // console.log(userInfo)
                     // 移除保存的房间用户信息
-                    var userSplice = ioRooms[rid].indexOf(userInfo);
-                    ioRooms[rid].splice(userSplice);
+                    var userSplice = ioRooms[rid].room.indexOf(userInfo);
+                    if(userSplice>-1){
+                         ioRooms[rid].room.splice(userSplice,1);
+                    }                    
                     // 告诉当前房间离开的用户
                     room.to(rid).emit('room sys', userInfo['username'] + '离开', ioRooms , ioRooms[rid]);
               })
