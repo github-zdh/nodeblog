cf9519bc87c10a3f77c50cc30dc6988144b6ada4
nodejs <2056401340@qq.com>
2018-05-31 12:00:45 +0800

    .

--- app/controller/search.js
+++ app/controller/search.js
@@ -89,6 +89,8 @@ exports.ChatRecord=function (req, res, next) {
 	      var page = req.body.page&&req.body.page>0 ? req.body.page-1 : 0;
 	      var pageSize = page*(req.body.pageSize?req.body.pageSize:10);	       
           var email = 'SELECT rm.* from z_room_msg rm WHERE rid='+rid+' LIMIT '+page+','+ pageSize;
+          console.log(email);
+          
           sql.runSql(email,function(err,data){
 		     	    if(err){
 		     	    	    return base.returnjson(res,100,"查询失败");
